497063ce9044c03e8db6ef246c2239bd9c4e53b5
Chris Bell <cjbell1988@gmail.com>
2014-05-05 15:22:14 -0400

    Store the country price in the Rails cache

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,13 +1,21 @@
 Spree::Variant.class_eval do
 
   def country_price(country_id)
-    prices.find_by(country_id: country_id)
+    Rails.cache.fetch(country_price_cache_key(country_id)) do
+      prices.find_by(country_id: country_id)
+    end
   end
 
   def country_price_or_default(country_id, currency)
     country_price(country_id) ||
     prices.find_by(currency: currency) ||
     default_price
+  end
+
+private
+
+  def country_price_cache_key(country_id)
+    "variant-#{id}-country-#{country_id}-price"
   end
 
 end
